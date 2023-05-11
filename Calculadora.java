1fc9e60ab9df001f9971217ee65912ebe77292bd
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-10 21:09:44 -0300

    Primeiros commits

---
+++ Calculadora.java
@@ -0,0 +1,27 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+				
+				System.out.println("Digite o nome do usuário:"); 
+				String nome = "Marcelo Nascimento";
+				System.out.println("Bem vindo(a)" + nome + "!");
+				System.out.println("_________________________");
+				System.out.println("Universidade Futuro");
+				System.out.println("_________________________");
+				System.out.println("Calculadora");
+				System.out.println("_________________________");
+				System.out.println("Digite [1] para soma:");
+				System.out.println("Digite [2] para subtração:");
+				System.out.println("Digite [3] para multiplicação:");
+				System.out.println("Digite [4] para divisão:");
+				
+				
+
+			}
+
+		
+
+	}
+
