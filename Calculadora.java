e435d51e8a3b4c9e752c2c9336956c5af35af63e
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-13 20:59:53 -0300

    Adicionando funcionalidades ao projeto

--- Calculadora.java
+++ Calculadora.java
@@ -25,13 +25,54 @@ public class Calculadora {
 				
 				if (valor == 1) {
 					System.out.println("Digite os valores das somas");
-					
-					
+					double soma1,soma2,somaTotal;
+					soma1 = teclado.nextDouble();
+					soma2 = teclado.nextDouble();
+					somaTotal = (soma1 + soma2);
+					System.out.println("A soma dos números é: " + somaTotal);
 				}
+				if(valor==2) {
+					System.out.println("Digite os valores da subtração:");
+					double sub1,sub2,subTotal;
+					sub1 = teclado.nextDouble();
+					sub2 = teclado.nextDouble();
+					subTotal = (sub1 - sub2);
+					System.out.println("O valor é :" + subTotal);
+				}
+				if(valor==3) {
+					System.out.println("Digite os valores da multiplicação:");
+					double multi1,multi2,multiTotal;
+					multi1 = teclado.nextDouble();
+					multi2 = teclado.nextDouble();
+					multiTotal = (multi1*multi2);
+					System.out.println("O valor é ; " + multiTotal);
+				}
+				if(valor==4) {
+					System.out.println("Digite os valores da divisão:");
+					double div1,div2,divTotal;
+					div1 = teclado.nextDouble();
+					div2 = teclado.nextDouble();
+					divTotal = (div1/div2);
+					System.out.println("O resultado da divisão é:" + divTotal);
+				}
+	
+	
+	}
+}
+						
+						
+			
 
-			}
+	
+					
+					
+					
+					
+					
+					
+				
 
 		
 
-	}
+	
 
